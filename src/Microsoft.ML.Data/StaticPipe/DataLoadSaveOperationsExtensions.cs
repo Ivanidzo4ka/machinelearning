006d0aa2f1b1cd3dc4ae0dfe824ad788dfb3eba1
Ivan Matantsev <ivmatan@microsoft.com>
2018-12-05 11:47:47 -0800

    Move exteansion static method to proper namespace.

--- src/Microsoft.ML.Data/StaticPipe/DataLoadSaveOperationsExtensions.cs
+++ src/Microsoft.ML.Data/StaticPipe/DataLoadSaveOperationsExtensions.cs
@@ -4,15 +4,7 @@
 
 using Microsoft.ML.Runtime;
 using Microsoft.ML.Runtime.Data;
-using Microsoft.ML.Runtime.Data.IO;
-using Microsoft.ML.Runtime.Internal.Utilities;
-using Microsoft.ML.StaticPipe;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using static Microsoft.ML.Runtime.Data.TextLoader;
 
 namespace Microsoft.ML.StaticPipe
 {
@@ -23,12 +15,12 @@ namespace Microsoft.ML.StaticPipe
         /// </summary>
         /// <typeparam name="TShape">The type shape parameter, which must be a valid-schema shape. As a practical
         /// matter this is generally not explicitly defined from the user, but is instead inferred from the return
-        /// type of the <paramref name="func"/> where one takes an input <see cref="Context"/> and uses it to compose
+        /// type of the <paramref name="func"/> where one takes an input <see cref="TextLoader.Context"/> and uses it to compose
         /// a shape-type instance describing what the columns are and how to load them from the file.</typeparam>
         /// <param name="catalog">The catalog.</param>
         /// <param name="func">The delegate that describes what fields to read from the text file, as well as
-        /// describing their input type. The way in which it works is that the delegate is fed a <see cref="Context"/>,
-        /// and the user composes a shape type with <see cref="PipelineColumn"/> instances out of that <see cref="Context"/>.
+        /// describing their input type. The way in which it works is that the delegate is fed a <see cref="TextLoader.Context"/>,
+        /// and the user composes a shape type with <see cref="PipelineColumn"/> instances out of that <see cref="TextLoader.Context"/>.
         /// The resulting data will have columns with the names corresponding to their names in the shape type.</param>
         /// <param name="files">Input files.</param>
         /// <param name="hasHeader">Data file has header with feature names.</param>
@@ -41,9 +33,21 @@ namespace Microsoft.ML.StaticPipe
         /// <param name="trimWhitspace">Remove trailing whitespace from lines.</param>
         /// <returns>A configured statically-typed reader for text files.</returns>
         public static DataReader<IMultiStreamSource, TShape> TextReader<[IsShape] TShape>(
-            this DataOperations catalog, Func<Context, TShape> func, IMultiStreamSource files = null,
+            this DataOperations catalog, Func<TextLoader.Context, TShape> func, IMultiStreamSource files = null,
             bool hasHeader = false, char separator = '\t', bool allowQuoting = true, bool allowSparse = true,
             bool trimWhitspace = false)
          => TextLoader.CreateReader(catalog.Environment, func, files, hasHeader, separator, allowQuoting, allowSparse, trimWhitspace);
+
+        /// <summary>
+        /// Read data from provided path(s).
+        /// </summary>
+        /// <typeparam name="TShape">The type shape parameter, which must be a valid-schema shape.</typeparam>
+        /// <param name="reader">Configured statically typed reader.</param>
+        /// <param name="path">Path to text file(s) to read data from.</param>
+        /// <returns>Dataview parameterized by <typeparamref name="TShape"/>.</returns>
+        public static DataView<TShape> Read<TShape>(this DataReader<IMultiStreamSource, TShape> reader, params string[] path)
+        {
+            return reader.Read(new MultiFileSource(path));
+        }
     }
 }
